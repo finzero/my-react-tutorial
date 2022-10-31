406de50e4b3266e4a76a6c64db06b6af8f2f42ee
Fazrin <fazrin.mutaqin@wgs-id.com>
2022-10-31 15:50:51 +0700

    prepare packages for tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="container-fluid mt-2 text-center">App.js</div>;
+  return <div>App.js</div>;
 }
 
 export default App;
